7bd6f654d4f4a7ae39bfae223eaa55ff4bb20f02
unknown <masumaakter.a1@gmail.com>
2024-02-19 14:54:44 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+const Names = ["Masuma", "Mazeda", "Nusrat"];
+
+Names.push("Basma")
+console.log(Names);
+
+var BagPrice = 3000;
+var myBudget = 1000;
+
+if(BagPrice< myBudget){
+    console.log("give me Bag");
+}else{
+    console.log("give me Pars")
+}
+
+
+
+const cars = ["Saab", "Volvo", "BMW"];
+
+cars.pop("BMW")
+console.log(cars);
+
+const fruits = ["mango", "orrange", "banana"];
+
+
+
+const name = {firstName:"mazeda", lastName:"akter", };
+
+
+
